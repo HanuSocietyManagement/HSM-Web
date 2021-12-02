0ce28a867bc2160eb5eda41dcc46c537b87453a2
Varun Verma <varunverma@varunverma.org>
2021-12-02 23:25:03 +0530

    Minor fixes

--- functions/src/routingAPI.ts
+++ functions/src/routingAPI.ts
@@ -2,9 +2,10 @@ import { Request, Response } from 'express';
 import { validateFirebaseIdToken } from './tokenExtractor';
 import { jugnu } from '@fire-fly/jugnu';
 import { User } from './model/user';
-import {CookieJar} from 'tough-cookie';
+import { CookieJar } from 'tough-cookie';
 import { wrapper } from 'axios-cookiejar-support';
 
+const CryptoJS = require("crypto-js");
 const axios = require('axios');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -48,7 +49,12 @@ app.post('/*', async (req: Request, res: Response) => {
 
 async function _doLogin(user: User){
 
-  const data = {user_name: user.temp.u, password: user.temp.p};
+  // Decrypt
+  const secret_key = process.env.SECRET_KEY;
+  const bytes  = CryptoJS.AES.decrypt(user.temp.p, secret_key);
+  const p = bytes.toString(CryptoJS.enc.Utf8);
+
+  const data = {user_name: user.temp.u, password: p};
   await axiosApi.post("/app/api/v1/login", data);
   //console.log("Login done", loginResponse.data);
   return;
